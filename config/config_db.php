fbf4bfc09670b774a30d2df266dccc0f1d34728d
Alex Korobov <korobovalex@mail.ru>
2019-06-29 17:04:26 +0300

    Файлы настроек

---
+++ config/config_db.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'dsn' => 'mysql:host=localhost;dbname=iShop; charset=utf8',
+    'user' => 'root',
+    'pass' => '',
+];
---
+++ config/params.php
@@ -0,0 +1,9 @@
+<?php
+
+    return [
+        'admin_email' => 'admin@mail.com',
+        'shop_name' => 'Магазин iShop',
+        'pagination' => 3,
+        'smtp_login' => '',
+        'smtp_password' => '',
+    ];
