a530af79d5c89a86f5d2117374d4dca2857bddf5
Alex Korobov <korobovalex@mail.ru>
2019-06-29 18:29:56 +0300

    Добавлены стили страницы

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -13,25 +13,8 @@ use ishop\Cache;
 
 class MainController extends AppController {
 
-
-
     public function indexAction() {
 
-        $posts = \R::findAll( 'test' );
-        $post = \R::findOne( 'test', 'id = ?', [2] );
-        debug( $post );
         $this->setMeta( App::$app->getProperty( 'shop_name' ), 'Описание...', 'Ключевые слова...' );
-        $name = 'Alex';
-        $age = 37;
-        $names = ['Andrey', 'Anna', 'Julia', 'Sasha'];
-        $cache = Cache::instance();
-        $cache->set( 'test', $names );
-//        $cache->delete( 'test' );
-        $data = $cache->get( 'test' );
-        if ( !$data ) {
-            $cache->set( 'test', $names );
-        }
-        debug( $data );
-        $this->set( compact('name', 'age', 'names', 'posts' ) );
     }
 }
--- app/views/Main/index.php
+++ app/views/Main/index.php
@@ -1,9 +1,1 @@
-<h1>Hello, world!</h1>
-
-<p><?= $name; ?></p>
-<p><?= $age; ?></p>
-<p><?= debug($names); ?></p>
-<?php foreach( $posts as $post ): ?>
-    <h3><?= $post->title; ?></h3>
-<?php endforeach; ?>
 
